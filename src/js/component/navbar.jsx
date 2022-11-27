1d01ab052f20b2ff64c5d2172d706eb56a2903b0
strps <csrstrps@gmail.com>
2022-11-27 05:19:47 +0000

    Stilying Navbar with bootstrap

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 class NavBar extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-light">
-                <div className="container-fluid" >
-                    <a className="navbar-brand" href="">Start Bootstrap</a>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div className="justify-content-between container-fluid" >
+                    <a className="navbar-brand text-white" href="">Start Bootstrap</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toogler-div" aria-controls="toogler-div" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="toogler-div">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav me-1 ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link active" href="#">Home</a>
                             </li>
