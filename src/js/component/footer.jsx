fcf3ae8d494bfa0053bcfd1d11b78db4305e8da6
strps <csrstrps@gmail.com>
2022-12-13 01:13:40 +0000

    fixing the footer position

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Footer extends React.Component {
     render() {
         return (
-            <div className="bg-dark py-5 text-white text-center">
+            <div className="bg-dark py-5 text-white text-center fixed-bottom">
                 <p className="my-0">Copyright © Your Website 2022</p>
             </div>
         )
